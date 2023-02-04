8f8a3eca84fcf64658d784f8c8b6373df6273ae7
Wiard van Rij <wvanrij@roku.com>
2023-02-04 22:07:25 +0100

    fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Fortio Tutorial - 5min ⏱️
+            to="/docs/category/getting-started">
+            Fortio Getting started - 1min ⏱️
           </Link>
         </div>
       </div>
